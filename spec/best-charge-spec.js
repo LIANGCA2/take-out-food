e94d57b5c366c1e343096688ccb81aaa7b9697e3
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-08 14:56:44 +0800

    finish test buildCartDishes and buildCartDishesWithDetailById

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,7 @@
+const {bestCharge} = require('../src/best-charge');
+const {loadAllItems} = require('../src/items');
+const {loadPromotions} = require('../src/promotions');
+const{buildCartDishes,buildCartDishesWithDetailById,calculateCartDishesOriginalPrice,getCartDishesPromotionPriceMap,calculateCartDishesPromotionPrice,choosePromotionWay,getPromotionPriceByPromotionType,calCartDishesBestCharge,generateReceipt} = require('../src/best-charge');
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
@@ -13,8 +17,8 @@ describe('Take out food', function () {
 指定菜品半价(黄焖鸡，凉皮)，省13元
 -----------------------------------
 总计：25元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+===================================`.trim();
+    expect(summary).toEqual(expected);
   });
 
   it('should generate best charge when best is 满30减6元', function() {
@@ -29,8 +33,8 @@ describe('Take out food', function () {
 满30减6元，省6元
 -----------------------------------
 总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+===================================`.trim();
+    expect(summary).toEqual(expected);
   });
 
   it('should generate best charge when no promotion can be used', function() {
@@ -41,8 +45,181 @@ describe('Take out food', function () {
 肉夹馍 x 4 = 24元
 -----------------------------------
 总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+===================================`.trim();
+    expect(summary).toEqual(expected);
   });
 
+
+  it('should return cartDishes specification Array', function() {
+    let inputs = ["ITEM0001 x 2","ITEM0013 x 1"];
+    let outputs = buildCartDishes(inputs);
+    let expected = [{
+      "id":"ITEM0001",
+      "count":"2"
+    },
+      {
+        "id":"ITEM0013",
+        "count":"1"
+      }];
+    expect(JSON.stringify(outputs)).toEqual(JSON.stringify(expected));
+  });
+
+  it('should return cartDishes Detail Array', function() {
+    let inputs = [{
+      "id":"ITEM0001",
+      "count":1
+    },
+      {
+        "id":"ITEM0030",
+        "count":2
+      }];
+    let allItemsArray = loadAllItems();
+    let outputs = buildCartDishesWithDetailById(inputs,allItemsArray);
+    let expected = [{
+      "id":"ITEM0001",
+      "name":"黄焖鸡",
+      "price":18.00,
+      "count":1
+    },
+      {
+        "id":"ITEM0030",
+        "name":"冰锋",
+        "price":2.00,
+        "count":2
+      }];
+    expect(JSON.stringify(outputs)).toEqual(JSON.stringify(expected));
+  });
+
+  it('should return originalPrice of cartDishes', function() {
+    let inputs = [{
+      "id":"ITEM0001",
+        "name":"黄焖鸡",
+      "price":18.00,
+        "count":2
+    },
+      {
+        "id":"ITEM0030",
+        "name":"冰锋",
+        "price":2.00,
+        "count":5
+      }];
+    let outputs = calculateCartDishesOriginalPrice(inputs);
+    let expected = 46;
+    expect(outputs).toEqual(expected);
+  });
+
+
+  it('should return cartDishes PromotionPriceMap', function() {
+    let inputs = [{
+      "id":"ITEM0001",
+      "name":"黄焖鸡",
+      "price":18.00,
+      "count":2
+    },
+      {
+        "id":"ITEM0030",
+        "name":"冰锋",
+        "price":2.00,
+        "count":5
+      }];
+    let promotionsArray = loadPromotions();
+    let outputs = getCartDishesPromotionPriceMap(inputs,promotionsArray);
+    let expected = [{
+      "promotionType":"满30减6元",
+      "promotionPrice":6
+    },{
+      "promotionType":"指定菜品半价",
+      "promotionPrice":18
+    }];
+    expect(JSON.stringify(outputs)).toEqual(expected);
+  });
+
+  it('should return cartDishes promotionPrice By calculateCartDishes', function() {
+    let cartDishesDetail = [{
+      "id":"ITEM0001",
+      "name":"黄焖鸡",
+      "price":18.00,
+      "count":2
+    },
+      {
+        "id":"ITEM0030",
+        "name":"冰锋",
+        "price":2.00,
+        "count":5
+      }];
+    let promotionsArray = loadPromotions();
+    let type = "满30减6元";
+    let outputs = calculateCartDishesPromotionPrice(cartDishesDetail,promotionsArray,type);
+    let expected = 6;
+    expect(outputs).toEqual(expected);
+  });
+
+
+  it('should return best PromotionWay', function() {
+    let promtionMap = [{
+      "promotionType":"满30减6元",
+        "promotionPrice":6
+    },{
+      "promotionType":"指定菜品半价",
+      "promotionPrice":18
+    }];
+    let outputs = choosePromotionWay(promtionMap);
+    let expected = `指定菜品半价`;
+    expect(JSON.stringify(outputs)).toEqual(expected);
+  });
+
+
+
+  it('should return promotionPrice by Type', function() {
+    let promotionMap = [{
+      "promotionType":"满30减6元",
+        "promotionPrice":6
+    },{
+      "promotionType":"指定菜品半价",
+      "promotionPrice":18
+    }];
+    let type = "指定菜品半价"
+    let outputs =getPromotionPriceByPromotionType(promotionMap,type);
+    let expected = 18;
+    expect(outputs).toEqual(expected)
+  });
+
+  it('should return bestCharge', function() {
+    let originalPrice = 46;
+    let promotionPrice = 18;
+    let outputs =calCartDishesBestCharge(originalPrice,promotionPrice);
+    let expected = 28;
+    expect(outputs).toEqual(expected);
+  });
+
+
+
+  it('should return receipt', function() {
+    let cartDishesDetail = [{
+      "id":"ITEM0001",
+      "name":"黄焖鸡",
+      "price":18.00,
+      "count":2
+    },
+      {
+        "id":"ITEM0030",
+        "name":"冰锋",
+        "price":2.00,
+        "count":5
+      }];
+    let bestCharge = 28;
+    let promotionType = "指定菜品半价";
+    let outputs =generateReceipt(cartDishesDetail,promotionType,bestCharge);
+    let expected = `
+============= 订餐明细 =============
+黄焖鸡 x 2 = 36元
+冰锋 x 5 = 10元
+-----------------------------------
+使用优惠:
+指定菜品半价，省18元
+-----------------------------------
+总计：28元
+===================================`.trim();
+    expect(JSON.stringify(outputs)).toEqual(expected);
+  });
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,62 @@
+
+const {loadAllItems} = require('./items');
+const {loadPromotions} = require('./promotions');
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  let allItemArray = loadAllItems();
+  let promotionArray = loadPromotions();
+  let cartDishesArray = buildCartDishes(allItemArray);
+  let cartDishesWithDetailArray = buildCartDishesWithDetailById(cartDishesArray,allItemArray);
+  let originalPrice = calculateCartDishesOriginalPrice(cartDishesWithDetailArray);
+  let promotionPriceMap = getCartDishesPromotionPriceMap(cartDishesArray,promotionArray);
+  let promotionType = choosePromotionWay(promotionPriceMap);
+  let promotionPrice = getPromotionPriceByPromotionType(promotionPriceMap,promotionType);
+  let bestCharge = calCartDishesBestCharge(originalPrice,promotionPrice);
+  let receipt = generateReceipt(cartDishesWithDetailArray,originalPrice,promotionType,originalPrice);
 }
+function buildCartDishes(allItemArray){
+//["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"]
+  let cartDishesArray =[] ;
+  allItemArray.forEach((item) =>{
+    const [id,count] = item.split("x");
+    cartDishesArray.push({
+      id:id.trim(),
+      count:count.trim()
+    })
+  });
+  return cartDishesArray;
+}
+function buildCartDishesWithDetailById(cartDishesArray,allItemArray){
+ let cartDishesWithDetailArray = [];
+ cartDishesArray.forEach((cartDish)=>{
+   const {id,name,price} = allItemArray.find((item)=>item.id ===cartDish.id);
+   cartDishesWithDetailArray.push({
+     id,
+     name,
+     price,
+     count:cartDish.count
+   })
+ })
+  return cartDishesWithDetailArray;
+}
+function calculateCartDishesOriginalPrice(cartDishesWithDetailArray){
+
+}
+function getCartDishesPromotionPriceMap(cartDishesArray,promotionArray){
+
+}
+function calculateCartDishesPromotionPrice(){
+
+}
+function choosePromotionWay(promotionPriceMap){
+
+}
+function getPromotionPriceByPromotionType(promotionPriceMap,promotionType){
+
+}
+function calCartDishesBestCharge(originalPrice,promotionPrice){
+
+}
+function generateReceipt(cartDishesWithDetailArray,originalPrice,promotionType,originalPrice){
+
+}
+module.exports = {bestCharge,buildCartDishes,buildCartDishesWithDetailById,calculateCartDishesOriginalPrice,getCartDishesPromotionPriceMap,calculateCartDishesPromotionPrice,choosePromotionWay,getPromotionPriceByPromotionType,generateReceipt}
